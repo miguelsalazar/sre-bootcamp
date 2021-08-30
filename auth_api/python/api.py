5a6f2104b854da0c97ecd47fb536e29e930b3c3b
Miguel Salazar <miguelsalazarg@gmail.com>
2021-08-30 18:20:03 -0500

    Refactoring code

--- auth_api/python/api.py
+++ auth_api/python/api.py
@@ -4,12 +4,9 @@ from flask import request
 from flask import abort
 from methods import Token, Restricted
 from os import environ
-from flask_mysqldb import MySQL
-import hashlib
-import jwt
 
 app = Flask(__name__)
-login = Token()
+login = Token(app)
 protected = Restricted()
 
 app.config['MYSQL_USER'] = environ.get("MYSQL_USER")
@@ -18,8 +15,6 @@ app.config['MYSQL_HOST'] = environ.get("MYSQL_HOST")
 app.config['MYSQL_DB'] = environ.get("MYSQL_DB")
 app.config['MYSQL_PORT'] = int(environ.get("MYSQL_PORT"))
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
-mysql = MySQL(app)
 
 # Just a health check
 @app.route("/")
@@ -38,25 +33,10 @@ def url_health():
 def url_login():
     username = request.form['username']
     password = request.form['password']
-
-    cur = mysql.connection.cursor()
-    query = f"SELECT * FROM users WHERE username='{username}';"
-    cur.execute(query)
-    results = cur.fetchall()
-
-    salt = results[0]['salt']
-    salted_input = str(password + salt).encode('utf-8')
-    h = hashlib.sha512()
-    h.update(salted_input)
-    hashed_password = h.hexdigest()
-    db_password = results[0]['password']    
-    role = results[0]['role']
-
-    res = {}
-    if hashed_password == db_password:
-        res = {
-            "data": login.generate_token(role)
-        }
+    res = {
+        "data": login.generate_token(username, password)
+    }
+    if res['data']:
         return jsonify(res)
     else:
         abort(403)
--- auth_api/python/methods.py
+++ auth_api/python/methods.py
@@ -1,21 +1,51 @@
-import jwt 
 from os import environ
+import hashlib
+from flask_mysqldb import MySQL
+import jwt 
+from flask import current_app
 
 # These functions need to be implemented
 class Token:
 
-    def generate_token(self, role):
-        try: 
-            payload = {
-                'role': role
-            }
-            return jwt.encode(
-                    payload,
-                    environ.get('JWT_SECRET_KEY'),
-                    algorithm='HS256'
-            )
-        except Exception as e:
-            return e 
+    def __init__(self, app):
+        self.mysql = MySQL(app)
+
+    def connect_to_db(self):
+        cur = self.mysql.connection.cursor()
+        return cur
+    
+    def query_user_data(self, cur, username):
+        query = f"SELECT * FROM users WHERE username='{username}';"
+        cur.execute(query)
+        results = cur.fetchall()
+        return results[0]
+    
+    def hash_password(self, salt, password):
+        salted_input = str(password + salt).encode('utf-8')
+        h = hashlib.sha512()
+        h.update(salted_input)
+        hashed_password = h.hexdigest()
+        return hashed_password
+
+    def generate_token(self, username, password):
+        cur = self.connect_to_db()
+        user = self.query_user_data(cur, username)
+        
+        hashed_password = self.hash_password(user['salt'], password)
+        db_password = user['password'] 
+
+        if hashed_password == db_password:
+            try: 
+                payload = {
+                    'role': user['role']
+                }
+                return jwt.encode(
+                        payload,
+                        environ.get('JWT_SECRET_KEY'),
+                        algorithm='HS256'
+                )
+            except Exception as e:
+                return e 
 
 
 class Restricted:
